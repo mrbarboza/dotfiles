81ed9dffc5bfefc4daa2d787736e3053008d23fc
Miguel Barboza <14060805+mrbarboza@users.noreply.github.com>
2024-07-22 12:40:59 -0300

    include nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,1 @@
+require("mrbarboza")
---
+++ nvim/lua/mrbarboza/plugins/colors.lua
@@ -0,0 +1,10 @@
+return {
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        opts = {},
+        config = function()
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+    },
+}
