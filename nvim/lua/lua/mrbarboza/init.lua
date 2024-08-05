5d1b584fd914979d04f3026a2e5b2763d702fb72
Miguel Barboza <14060805+mrbarboza@users.noreply.github.com>
2024-08-05 14:15:25 -0300

    change nvim config

--- nvim/lua/lua/mrbarboza/init.lua
+++ nvim/lua/lua/mrbarboza/init.lua
@@ -4,7 +4,7 @@ require("mrbarboza.lazy")
 
 vim.g.netrw_browse_split = 0
 vim.g.netrw_banner = 0
-vim.g.netrw_winsize = 25
+vim.g.netrw_altv = 1
 
 vim.opt.listchars = { eol = "↵", tab = "→  ", trail = "·", extends = "$" }
 vim.opt.list = true
--- nvim/lua/lua/mrbarboza/remap.lua
+++ nvim/lua/lua/mrbarboza/remap.lua
@@ -4,6 +4,8 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
+vim.keymap.set("n", "ss", ":split<Return>")
+vim.keymap.set("n", "sv", ":vsplit<Return>")
 
 vim.keymap.set("n", "<leader><leader>", function()
     vim.cmd("so")
